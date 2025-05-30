4385da23bf49249a8b8c179bc9c3fd6c54bdc321
rickgrimes70 <rickgrimes4332@gmail.com>
2025-05-30 19:03:06 +0700

    a

---
+++ example/lib/views/response_center_screen.dart
@@ -0,0 +1,37 @@
+import 'package:example/data/info_list.dart';
+import 'package:flutter/material.dart';
+import 'package:responsive_phk/widgets/responsive_center.dart';
+
+class ResponseCenterScreen extends StatelessWidget {
+  const ResponseCenterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+        centerTitle: true,
+      ),
+      body: ResponsiveCenter(
+        padding: EdgeInsets.all(10),
+        child: ListView.builder(
+            itemCount: info.length,
+            itemBuilder: (context, index) {
+              return Card(
+                elevation: 5,
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(info[index].title ?? ''),
+                      Text(info[index].description)
+                    ],
+                  ),
+                ),
+              );
+            }),
+      ),
+    );
+  }
+}
---
+++ example/lib/views/response_two_column_screen.dart
@@ -0,0 +1,62 @@
+import 'package:example/data/info_list.dart';
+import 'package:flutter/material.dart';
+import 'package:responsive_phk/constants/breakpoints.dart';
+import 'package:responsive_phk/widgets/responsive_two_column_layout.dart';
+
+class ResponsiveTwwColumnScreen extends StatelessWidget {
+  const ResponsiveTwwColumnScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Home Screen'),
+          centerTitle: true,
+        ),
+        body: Align(
+          alignment: Alignment.topCenter,
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: SizedBox(
+              width: Breakpoint.desktop,
+              child: ResponsiveTwoColumnLayout(
+                startContent: Container(
+                  height: 200,
+                  width: double.infinity,
+                  decoration: BoxDecoration(color: Colors.blueGrey),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Pyae Htoo Kyaw'),
+                      Text('phk99977@gmail.com'),
+                      Text('I am senior software developer.')
+                    ],
+                  ),
+                ),
+                spacing: 20,
+                endContent: Expanded(
+                  child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: info.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          elevation: 5,
+                          child: Padding(
+                            padding: EdgeInsets.all(20),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(info[index].title ?? ''),
+                                Text(info[index].description)
+                              ],
+                            ),
+                          ),
+                        );
+                      }),
+                ),
+              ),
+            ),
+          ),
+        ));
+  }
+}
