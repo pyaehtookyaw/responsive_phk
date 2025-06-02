cb27d15d772693fe283378b9e6de62fe535005d9
rickgrimes70 <rickgrimes4332@gmail.com>
2025-06-02 18:13:01 +0700

    a

--- example/lib/data/info_list.dart
+++ example/lib/data/info_list.dart
@@ -2,6 +2,6 @@ import 'package:example/models/info.dart';
 
 List info = [
   Info(title: 'Name', description: 'Pyae Htoo Kyaw'),
-  Info(title: 'Email', description: 'pho99977@gmail.com'),
+  Info(title: 'Email', description: 'phk99977@gmail.com'),
   Info(title: 'Position', description: 'Senior developer'),
 ];
